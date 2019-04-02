6dd11a6b7e82fa56ed5fb32d7a6e7a84016c2b27
Jackson-p <1216884492@qq.com>
2019-04-02 16:30:44 +0800

    waitsocket

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDom from 'react-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Login from './container/login/login'
 import Register from './container/register/register'
 import AuthRoute from './component/authroute/authroute'
+import BossInfo from './container/bossinfo/bossinfo'
+import GeniusInfo from './container/geniusinfo/geniusinfo'
+import Dashboard from './component/dashboard/dashboard'
 import reducers from './reducer'
 import './config'
 import './index.css'
@@ -16,17 +19,22 @@ const store = createStore(reducers, compose(
 	applyMiddleware(thunk),
 	window.__REDUX_DEVTOOLS_EXTENSION__?window.__REDUX_DEVTOOLS_EXTENSION__():f=>f
 ))
-function Boss(){
-	return <h2>BOSS页面</h2>
-}
+
+// boss genius me msg 4个页面
 ReactDom.render(
 	(<Provider store={store}>
 		<BrowserRouter>
 			<div>
 				<AuthRoute></AuthRoute>
-				<Route path='/boss' component={Boss}></Route>
-				<Route path='/login' component={Login}></Route>
-				<Route path='/register' component={Register}></Route>
+				<Switch>
+					
+					<Route path='/bossinfo' component={BossInfo}></Route>
+					<Route path='/geniusinfo' component={GeniusInfo}></Route>
+					<Route path='/login' component={Login}></Route>
+					<Route path='/register' component={Register}></Route>
+					<Route component={Dashboard}></Route>
+				</Switch>
+
 			</div>
 		</BrowserRouter>
 	</Provider>),
